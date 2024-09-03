aade5abf00aaa921051ee393bce2ecfb7b004824
Shivam Gutgutia <gutgutia159@gmail.com>
2024-09-03 12:16:42 +0530

    Changed inv to pinv to work even for non singular matrix

--- algorithms/linear_regression.py
+++ algorithms/linear_regression.py
@@ -1,16 +1,16 @@
 import numpy as np
 
+
 class LinearRegression:
     def __init__(self):
-        self.coeff=None
-        self.intercept=None
+        self.coeff = None
+        self.intercept = None
 
-    def fit(self,xtrain,ytrain):
-        xtrain = np.insert(xtrain,0,1,axis=1)
-        betas = np.linalg.inv(np.dot(xtrain.T,xtrain)).dot(xtrain.T).dot(ytrain)
+    def fit(self, xtrain, ytrain):
+        xtrain = np.insert(xtrain, 0, 1, axis=1)
+        betas = np.linalg.pinv(np.dot(xtrain.T, xtrain)).dot(xtrain.T).dot(ytrain)
         self.coeff = betas[1:]
         self.intercept = betas[0]
 
-    def predict(self,xtest):
-        return np.dot(xtest,self.coeff.T)+self.intercept
-    
+    def predict(self, xtest):
+        return np.dot(xtest, self.coeff.T) + self.intercept
